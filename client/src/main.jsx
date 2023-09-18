ebf7d686b41611e78f16068d7c357cb2ebd9678f
Shahid Afridi <shafridi9807@gmail.com>
2023-09-18 16:27:09 +0530

    changing route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// export const server = "https://dall-e-clone-ag43.onrender.com/api/v1";
-export const server = "http://localhost:3000/api/v1";
+export const server = "https://dall-e-clone-ag43.onrender.com/api/v1";
+// export const server = "http://localhost:3000/api/v1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
